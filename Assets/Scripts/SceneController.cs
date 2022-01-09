23d4cbc6abd4c3ad3f8f197d66a56b43d586f4a8
AlexCS04 <alexcanosala@gmail.com>
2022-01-09 20:33:25 +0100

    Create SceneController.cs

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public int LevelTLoad;
+    public bool LoadLevel = false;
+    public static int NextLevel;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NextLevel == 5)
+        {
+            LoadLevel = true;
+        }
+        else
+        {
+            LoadLevel = false;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        GameObject collisionGameObject= collision.gameObject;
+        if(collisionGameObject.name == "ruby")
+        {
+            LoadScene();
+            NextLevel = 0;
+
+        }
+    }
+    void LoadScene()
+    {
+        if (LoadLevel)
+        {
+            SceneManager.LoadScene(LevelTLoad);
+            
+        }
+
+    }
+    
+}
+
